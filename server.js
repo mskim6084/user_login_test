53bed4577423703e11e0c1249bd44bb6ded559dc
MinSung Kim <kimms@myumanitoba.ca>
2022-05-30 15:18:25 -0500

    creating code to be able to take input, generate uuid and salt and hashing passwords

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+
+const users = []
 
 app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req,res) => {
-    console.log('http://localhost:3000')
     res.render('homepage')
 })
 
-app.listen(3000)
+app.get('/register', (req,res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req,res) => {
+    try{
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const user_id = uuidv4();
+        const user = { id: user_id, name: req.body.name, email:req.body.email, password: hashedPassword}
+        users.push(user)
+    }
+    catch{
+        res.status(500).send("Uh-oh")
+    }
+
+    console.log(users)
+    
+})
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000')
+})
